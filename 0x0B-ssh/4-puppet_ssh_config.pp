53404a180a75666839d5edebaa65d95f4b2d01d0
rmf10003 <685@holbertonschool.com>
2019-08-07 21:25:23 +0000

    append on no match

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,15 @@
 # automate changes to ssh_config using puppet
 file_line { 'pass?':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => 'PasswordAuthentication no',
-    match  => '^#?\s*PasswordAuthentication\syes'
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => 'PasswordAuthentication no',
+    match   => '^#?\s*PasswordAuthentication\syes'
+    replace => true,
 }
 file_line { 'pub_key':
-    ensure            => present,
-    path              => '/etc/ssh/ssh_config',
-    line              => 'IdentifyFile ~/.ssh/holberton',
-    match             => '^#?\s*IdentifyFile ~/.ssh/holberton',
-    match_for_absence => true,
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => 'IdentifyFile ~/.ssh/holberton',
+    match              => '^#?\s*IdentifyFile ~/.ssh/holberton',
+    append_on_no_match => true,
 }
