6a1bd3e90c2ae0ce12fe425cfe18b5d37313bd17
rmf10003 <685@holbertonschool.com>
2019-08-07 21:28:48 +0000

    fix

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -3,13 +3,13 @@ file_line { 'pass?':
     ensure  => present,
     path    => '/etc/ssh/ssh_config',
     line    => 'PasswordAuthentication no',
-    match   => '^#?\s*PasswordAuthentication\syes'
+    match   => '^#?.*PasswordAuthentication yes'
     replace => true,
 }
 file_line { 'pub_key':
     ensure             => present,
     path               => '/etc/ssh/ssh_config',
     line               => 'IdentifyFile ~/.ssh/holberton',
-    match              => '^#?\s*IdentifyFile ~/.ssh/holberton',
+    match              => '^#?.*IdentifyFile ~/.ssh/holberton',
     append_on_no_match => true,
 }
