75f4a2ae9d0319f2eb1dbc6d365aa3a3c64f232f
rmf10003 <685@holbertonschool.com>
2019-08-07 21:21:36 +0000

    make changes after update to server

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,14 @@
-#!/usr/bin/env bash
+# automate changes to ssh_config using puppet
+file_line { 'pass?':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+    match  => '^#?\s*PasswordAuthentication\syes'
+}
+file_line { 'pub_key':
+    ensure            => present,
+    path              => '/etc/ssh/ssh_config',
+    line              => 'IdentifyFile ~/.ssh/holberton',
+    match             => '^#?\s*IdentifyFile ~/.ssh/holberton',
+    match_for_absence => true,
+}
