50eaa1f6f2223c2f56dc3dc4e4bf8772270f351b
rmf10003 <685@holbertonschool.com>
2019-08-07 21:36:01 +0000

    NFix

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,18 @@
 # automate changes to ssh_config using puppet
-file_line { 'pass?':
-    ensure  => present,
-    path    => '/etc/ssh/ssh_config',
-    line    => 'PasswordAuthentication no',
-    match   => '^#?.*PasswordAuthentication yes'
-    replace => true,
+file_line { 'password':
+    ensure   => present,
+    line     => 'PasswordAuthentication no',
+    match    => '^.?.*PasswordAuthentication yes$',
+    replace  => true,
+    path     => '/etc/ssh/ssh_config',
+    multiple => false,
 }
-file_line { 'pub_key':
+
+file_line { 'identity':
     ensure             => present,
+    line               => 'IdentityFile ~/.ssh/holberton',
+    match              => '^.*IdentityFile ~/.ssh/holberton$',
     path               => '/etc/ssh/ssh_config',
-    line               => 'IdentifyFile ~/.ssh/holberton',
-    match              => '^#?.*IdentifyFile ~/.ssh/holberton',
+    multiple           => false,
     append_on_no_match => true,
 }
