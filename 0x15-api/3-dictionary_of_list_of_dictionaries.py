fb6d764647538d495043b5da979a2d65f7499b75
rmf10003 <685@holbertonschool.com>
2019-09-17 00:56:46 +0000

    create python script that formats all user tasks into json file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,37 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+"""module for accessing JSONPlaceholder api"""
+
+
+import json
+import requests
+
+
+if __name__ == '__main__':
+    id_list = []
+    emp_list = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+    for user in emp_list:
+        id_list.append(user.get('id'))
+    id_list.sort()
+    ret = {}
+    for id in id_list:
+        response_todos = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(id)
+        )
+        emp = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'
+            .format(id)
+        ).json()
+        emp_tasks = response_todos.json()
+        tasks_list = []
+        for task in emp_tasks:
+            inner_dict = {
+                "username": emp.get('username'),
+                "task": task.get('title'),
+                "completed": task.get('completed')
+            }
+            tasks_list.append(inner_dict)
+        ret[id] = tasks_list
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(ret, f)
