f021abae04d87c365ff06696871577911d27e245
rmf10003 <685@holbertonschool.com>
2019-09-19 19:54:01 +0000

    put an authentic user agent

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ import requests
 
 def recurse(subreddit, hot_list=[], count=0, after=''):
     """recursively return list containing hot post titles for a given sub"""
-    user_agent = {'User-agent': 'Mozilla/5.0'}
+    user_agent = {'User-agent': 'python:this_is_rory:1.0'}
     url = "https://www.reddit.com/r/{}/hot.json?after={}&count={}".format(
         subreddit,
         after,
@@ -15,12 +15,13 @@ def recurse(subreddit, hot_list=[], count=0, after=''):
     resp = requests.get(url, allow_redirects=False, headers=user_agent)
     if resp.status_code in (302, 404):
         return None
-    if len(resp.json().get('data').get('children')) == 0:
-        if count == 0:
-            return None
+    resp_json = resp.json()
+    after = resp_json.get('data').get('after')
+    count += resp_json.get('data').get('dist')
+    if count == 0:
+        return None
+    for post in resp_json.get('data').get('children'):
+        hot_list.append(post.get('data').get('title'))
+    if after is None:
         return hot_list
-    after = resp.json().get('data').get('after')
-    count += resp.json().get('data').get('dist')
-    for post in resp.json().get('data').get('children'):
-            hot_list.append(post.get('data').get('title'))
-    recurse(subreddit, hot_list=hot_list, count=count, after=after)
+    return recurse(subreddit, hot_list=hot_list, count=count, after=after)
