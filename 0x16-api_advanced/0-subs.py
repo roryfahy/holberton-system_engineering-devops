f25aa64837170af5ceee4ad43a4da0563f6ec5aa
rmf10003 <685@holbertonschool.com>
2019-09-19 17:46:03 +0000

    redirect with 302 error code

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""query reddit api for num of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribrers of a given subreddit"""
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    resp = requests.get(url, allow_redirects=False, headers=user_agent)
+    if resp.status_code == 302:
+        return 0
+    return resp.json().get('data').get('subscribers')
