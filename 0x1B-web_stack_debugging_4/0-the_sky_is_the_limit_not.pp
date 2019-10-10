037a6eb6f9790db6650a2e51a29956d2acc8805a
rmf10003 <685@holbertonschool.com>
2019-10-10 19:08:18 +0000

    add block to restart nginx server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,3 +6,6 @@ file { 'resource title':
             group   => 'root',
             content => 'ULIMIT="-n 15000"\n',
             }
+exec { 'not resource title':
+          command => '/usr/bin/sudo /usr/sbin/service nginx restart'
+            }
