8e56545717204305e5f08981b7a89cb8f4389cfa
rmf10003 <685@holbertonschool.com>
2019-10-10 19:02:05 +0000

    pass puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
+# this file is used to increase the OS limit of nginx open file count
 file { 'resource title':
-       path    => '/etc/default/nginx',
-       mode    => '0644',
-       owner   => 'root',
-       group   => 'root',
-       content => 'ULIMIT="-n 15000"\n',
-     }
+            path    => '/etc/default/nginx',
+            mode    => '0644',
+            owner   => 'root',
+            group   => 'root',
+            content => 'ULIMIT="-n 15000"\n',
+            }
